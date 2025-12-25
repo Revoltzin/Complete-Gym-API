46f6c34e088cbc3825dc88b297508e0c9ce73347
Revoltzin <jat1963@gmail.com>
2025-12-25 20:17:10 -0300

    Added the routes & JSON permission usage

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const routes = require("./routes/routes.js")
 
 const app = express()
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server Online! ")
